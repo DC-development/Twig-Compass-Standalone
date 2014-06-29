03d6725431e6853052fb80938483cdbcd970eb80
philipp.sengelmann <p.sengelmann@bitforce-it.de>
2014-06-29 23:50:35 +0200

    added external data for carousel - "twigged" carousel component. added some other components as html snippits for testing.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
 
 require_once './vendor/autoload.php';
+require_once './mockup_objects/data.php';
 
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader, array(
@@ -14,4 +18,4 @@ if(isset($_GET['p'])) {
     $page = "forms";
 }
 
-echo $twig->render("$page.html.twig", array('name' => 'Fabien, my friend'));
+echo $twig->render("$page.html.twig", array('sliderData' => $sliderData));
---
+++ mockup_objects/data.php
@@ -0,0 +1,23 @@
+<?php
+
+$sliderData = [
+    "slide1" => [
+        "image" => "holder.js/1140x280/auto/#9BF22B:#31dded",
+
+        "target" => "path",
+        "btntext" => "No caption defined"
+    ],
+    "slide2" => [
+        "image" => "holder.js/1140x280/auto/#31dded:#9BF22B",
+        "caption" => "Sign In",
+        "text" => "text",
+        "target" => "path",
+        "btntext" => "Sign In"
+    ],
+    "slide3" => [
+        "image" => "holder.js/1140x280/auto/#712de5:#31dded",
+        "caption" => "No button defined",
+        "text" => "text",
+
+    ]
+];
